61157103a9eb2363f7ad1bb5318856ef55e1ec31
Kseniya Kochneva <ks3nya1337@gmail.com>
2021-05-22 00:46:43 +0300

    fix file models init

--- web/hltv_parser/__init__.py
+++
@@ -1,7 +0,0 @@
-from .models.match import Match
-from .models.team import Team
-from .models.user import User
-from .models.match_map import MatchMap
-from .models.map import Map
-from .models.match_veto import MatchVeto
-from .models.tournament import Tournament
---
+++ web/hltv_parser/models/__init__.py
@@ -0,0 +1,7 @@
+from .match import Match
+from .team import Team
+from .user import User
+from .match_map import MatchMap
+from .map import Map
+from .match_veto import MatchVeto
+from .tournament import Tournament
